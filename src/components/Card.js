3d263cadf09769d5925fe910785d3d84592b8cb4
igips <igor.szepietowski@gmail.com>
2022-02-08 20:33:44 +0100

    Design Card

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import "../style/Card.css";
+
+
+function Card(props) {
+
+
+    return(
+        <div className="pictureDiv"><img src={require(`../img/${props.src}.jpeg`)} alt="" /><div className="picName">{props.name}</div></div>
+    );
+}
+
+export default Card;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import  "../style/Header.css";
+
+function Header() {
+
+    return (
+        <header >Architecture Memory Game</header>
+    );
+} 
+
+export default Header;
