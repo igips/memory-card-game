142b11c1abff24b092da9863feca2ae10bc10240
igips <igor.szepietowski@gmail.com>
2022-02-09 16:20:45 +0100

    Add props

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,7 +16,7 @@ function Card(props) {
             a.removeEventListener("click", props.click);
         };
 
-    });
+    },[props]);
 
     return(
         <div ref={ref} className="pictureDiv"><img src={require(`../img/${props.src}.jpeg`)} alt="" /><div className="picName">{props.name}</div></div>
